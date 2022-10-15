9f474f17fc65c9304e9655612e44e4e1c7caabfe
Brett Kohler <brettkohler93@gmail.com>
2022-10-15 14:40:29 -0700

    switch riverpod to flutter_riverpod

--- lib/single_player_game/presentation/single_player_page.dart
+++ lib/single_player_game/presentation/single_player_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:riverpod/riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class SinglePlayerPage extends ConsumerWidget {
   const SinglePlayerPage({required this.title}): super(key: key);
