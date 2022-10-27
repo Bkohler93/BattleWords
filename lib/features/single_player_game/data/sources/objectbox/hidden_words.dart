e99dc308e79c0ac06ca9135c79e9e4c12fb59858
Brett Kohler <brettkohler93@gmail.com>
2022-10-27 15:12:10 -0700

    added objectbox and configured project to use it

---
+++ lib/features/single_player_game/data/sources/objectbox/hidden_words.dart
@@ -0,0 +1,4 @@
+//TODO set up ObjectBox as database. Figure out how to set up that class or whatever... 
+
+
+//TODO Create method that reads in txt file encoded as a JSON string and decodes it into JSON. Then each word will be placed into the database, with each word having two fields: Text and length.
