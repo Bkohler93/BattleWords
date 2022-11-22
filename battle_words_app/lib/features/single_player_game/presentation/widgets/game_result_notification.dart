ad5b66152ea5dcef90a290e7222ac49d1393ae1e
Brett Kohler <brettkohler93@gmail.com>
2022-11-22 12:28:49 -0800

    use Align widget to center positioned elements in Single Player Game screen

--- battle_words_app/lib/features/single_player_game/presentation/widgets/game_result_notification.dart
+++ battle_words_app/lib/features/single_player_game/presentation/widgets/game_result_notification.dart
@@ -1,6 +1,5 @@
 import 'package:battle_words/features/single_player_game/domain/hidden_word.dart';
 import 'package:battle_words/features/single_player_game/presentation/controllers/guess_input.dart';
-import 'package:battle_words/features/single_player_game/presentation/controllers/keyboard_letters.dart';
 import 'package:battle_words/features/single_player_game/presentation/controllers/single_player_game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -14,8 +13,8 @@ class GameResultNotification extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Container(
-      height: 30.h,
-      width: 50.w,
+      height: 40.h,
+      width: 70.w,
       color: Colors.black87,
       child: Padding(
         padding: EdgeInsets.symmetric(vertical: (8.h).toDouble()),
@@ -24,17 +23,38 @@ class GameResultNotification extends ConsumerWidget {
           children: [
             Text(
               result,
-              style: TextStyle(color: Colors.white),
+              style: const TextStyle(color: Colors.white),
             ),
+            (hiddenWords.any((hiddenWord) => !hiddenWord.isWordFound))
+                ? Column(children: [
+                    Text("Words you missed",
+                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: List.generate(
+                        hiddenWords.length,
+                        (index) => hiddenWords[index].isWordFound
+                            ? SizedBox(
+                                height: 1,
+                                width: 1,
+                              )
+                            : Text('${hiddenWords[index].word}',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                )),
+                      ),
+                    )
+                  ])
+                : SizedBox(),
             Padding(
               padding: const EdgeInsets.all(12.0),
               child: FloatingActionButton.extended(
                 onPressed: () {
-                  // ref.invalidate(singlePlayerGameControllerProvider);
-                  // ref.invalidate(guessWordInputControllerProvider);
+                  ref.invalidate(singlePlayerGameControllerProvider);
+                  ref.invalidate(guessWordInputControllerProvider);
                   Navigator.of(context).pop();
                 },
-                label: Text("Main Menu"),
+                label: const Text("Main Menu"),
               ),
             )
           ],
