0a780938aeae8258c7f71d3a8d0840332d3512c3
bkohler93 <brettkohler93@gmail.com>
2023-01-06 23:27:28 -0800

    made changes before creating apk

--- battle_words_app/lib/main.dart
+++ battle_words_app/lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:battle_words/firebase_options.dart';
 import 'package:battle_words/src/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
--- battle_words_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ battle_words_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 import objectbox_flutter_libs
 import path_provider_macos
 import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   ObjectboxFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "ObjectboxFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
