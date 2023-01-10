61cab4ce195a15d58e4181541123ac9359184022
Brett Kohler <brettkohler93@gmail.com>
2023-01-10 14:17:52 -0800

    removed firebase imports

--- battle_words_app/lib/main.dart
+++ battle_words_app/lib/main.dart
@@ -1,12 +1,8 @@
-import 'package:battle_words/firebase_options.dart';
 import 'package:battle_words/src/app.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
 }
